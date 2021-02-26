9b1579ac4c32f8c31e9d748edd6f0af227cd3238
bcifuentes <bcifuentes@unal.edu.co>
2021-02-26 12:34:44 -0500

    ahora recibe datos de la consola

--- 2021-26-02HER/ejemplo.cpp
+++ 2021-26-02HER/ejemplo.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <cstdlib>
 
 int main(int argc, char **argv){
   int a = std::atoi(argv[1]);
   std::cout << "Hola mundo !\n";
-
+  std::cout << a<<"\n";
   return 0;
 }
