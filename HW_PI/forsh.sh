0e04b3fdc4b9a6cf0ba082232adf487601e6b2c5
Estudiante15 Pr_wfoquendo <woquendo_St15@hercules6.biocomputo.unal.edu.co>
2021-05-30 11:44:31 -0500

    datos

--- HW_PI/forsh.sh
+++ HW_PI/forsh.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-for i in 1 2 3 4 5 6 7 8 9 10
+for i in 1 2 3 4 5
 do
-   mpirun -np $i -oversubscribe ./a.out 2000000000 >> datos.txt
+   for N in 1 2 3 4 5 6 7 8 9 10 11
+   do
+      mpirun -np $N -oversubscribe ./a.out 2000000000 >> datos$i.txt
+   done
 done
