0adb1bfab94ce6d52c62900188c6308087f5bd66
Jean Pierre Cifuentes <bcifuentes@unal.edu.co>
2021-05-30 12:05:00 -0500

    pup

--- HW_PI/forsh.sh
+++ HW_PI/forsh.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-for i in 1 2 3 4 5
+for N in 1 2 3 4 5 6 7 8 9 10 11
 do
-   for N in 1 2 3 4 5 6 7 8 9 10 11
-   do
-      mpirun -np $N -oversubscribe ./a.out 2000000000 >> datos$i.txt
-   done
+  mpirun -np $N -oversubscribe ./a.out 2000000000 >> data.txt
 done
+
--- HW_PI/loop.sh
+++
@@ -1,3 +0,0 @@
-for N in 1 2 3 4 5 6 7 8 9 10; do \
-	mpirun -np N -oversubscribe ./a.out 2000000000 >> data.txt ;\
-done ;\
